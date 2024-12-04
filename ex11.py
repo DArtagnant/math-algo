249e28aa520dd7226b5b238614128aabf7d79756
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-12-04 19:16:02 +0100

    ex 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+
+a = list(range(17))
+b = [e for e in range(30, 9, -1)]
+c = [n for n in range(12) for _ in range(3)]
+d = [n for n in range(12) for _ in range(n)]
+
+print(a)
+print(b)
+print(c)
+print(d)
